454c1527fd878d2ef763922087b25a46cc4816dd
okantas <okantas954@gmail.com>
2023-06-28 00:35:51 +0300

    adding functionallity to the buttom

---
+++ index.js
@@ -0,0 +1,11 @@
+
+let myLeads=[]
+
+let inputEl =document.querySelector("#input-el")
+let inputBtn = document.querySelector("#input-btn")
+
+inputBtn.addEventListener("click",function(){
+
+    console.log("Button Clicked !!!")
+
+})
