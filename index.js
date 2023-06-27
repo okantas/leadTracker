c3f03f4a3f01c2937b8c583d7da34caece032b85
okantas <okantas954@gmail.com>
2023-06-28 01:35:48 +0300

    getting the value from input

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 
-let myLeads=[]
+let myLeads=["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 
 let inputEl =document.querySelector("#input-el")
 let inputBtn = document.querySelector("#input-btn")
 
+
 inputBtn.addEventListener("click",function(){
 
-    console.log("Button Clicked !!!")
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
 
 })
+
+for(let i = 0; i < myLeads.length; i++){
+    console.log(myLeads[i])
+}
