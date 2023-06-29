3f471720190746524aca03ede26777d575899b69
okantas <okantas954@gmail.com>
2023-06-29 11:41:00 +0300

    part of local storage is done

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ const inputEl =document.querySelector("#input-el")
 const inputBtn = document.querySelector("#input-btn")
 const ulEl = document.querySelector("#ul-el")
 
-
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads")) 
+console.log( leadsFromLocalStorage)
 
 inputBtn.addEventListener("click",function(){
 
     myLeads.push(inputEl.value)
     inputEl.value = " "
+    
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     renderLeads()
+    console.log( localStorage.getItem("myLeads") )
 })
 
 function renderLeads(){
